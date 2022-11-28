6337684ee211eb51ed4106f0eb6d8bf80b98754b
luisc68 <116054815+luisc68@users.noreply.github.com>
2022-11-28 20:00:56 +0000

    librerias

---
+++ main.py
@@ -0,0 +1,22 @@
+#IMPORTACIONES DE LIBRERIAS
+import pyttsx3
+import docx2txt
+from pytube import YouTube
+import download
+import os
+import moviepy
+from moviepy.editor import *
+
+import time
+import schedule
+import time
+import random
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager as CM
