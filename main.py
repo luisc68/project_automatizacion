57de3a7ecd2bbaa71cb6d8862880dc368b1a5f0a
luisc68 <116054815+luisc68@users.noreply.github.com>
2022-11-28 20:14:16 +0000

    agregar la parte de obtener audio

--- main.py
+++ main.py
@@ -20,3 +20,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager as CM
+
+def obteneraudio():
+    
+    print("------------Obtener Audio-------------")
+    print()
+    print("//////////////////////////////////////")
+    print()
+    print("------Convirtiendo Texto a Audio------")
+    engine = pyttsx3.init()
+    #Leer archivo de word
+    my_text = docx2txt.process("text.docx")
+    #Guardar audio
+    engine.save_to_file(my_text, 'audio2.mp3')
+    engine.runAndWait()
+    print()
+    print("--------------Audio Listo-------------")
+    print()
